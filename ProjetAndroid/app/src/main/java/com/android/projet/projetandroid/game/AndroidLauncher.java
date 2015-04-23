5f0e9a5e19c9e97ace722db012b6cfb2896e3881
aecrepont <adrien.ecrepont@outlook.com>
2015-04-23 01:20:53 -0400

    libgdx

---
+++ ProjetAndroid/app/src/main/java/com/android/projet/projetandroid/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.android.projet.projetandroid.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.android.projet.projetandroid.game.superjumper.SuperJumper;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SuperJumper(), config);
+	}
+}
